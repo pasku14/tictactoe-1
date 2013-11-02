0b7692025eb8c8ab1e8f3477e459bd5a3dc6830c
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-11-02 17:35:17 +0000

    almost operative

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ use Rack::Session::Pool, :expire_after => 2592000
 set :session_secret, 'super secret'
 set :sessions, :domain => 'example.com'
 
+module TicTacToe
+  CIRCLE = "circle"
+  CROSS  = "cross"
+  BLANK  = ""
+end 
+
 class Board
+include TicTacToe
 
   HORIZONTALS = [ %w{a1 a2 a3},  %w{b1 b2 b3}, %w{c1 c2 c3} ]
   COLUMNS     = [ %w{a1 b1 c1},  %w{a2 b2 c2}, %w{a3 b3 c3} ]
@@ -19,7 +26,7 @@ class Board
     @session = session
     @session["board"] = {}
     MOVES.each do |k|
-      @session["board"][k] = ""
+      @session["board"][k] = BLANK
     end
   end
 
@@ -38,31 +45,34 @@ class Board
   def legal_moves
     m = []
     MOVES.each do |key|
-      m << key unless @session["board"][key].length > 0
+      m << key if @session["board"][key] == BLANK
     end
     puts "legal_moves: Tablero:  #{board.inspect}"
     puts "legal_moves: m:  #{m}"
     m # returns the set of feasible moves [ "b3", "c2", ... ]
   end
 
-  def won?
+  def winner
     ROWS.each do |row|
-      return "cross" if row.xs == 3 # "cross" wins
-      return "circle" if row.os == 3 # "circle" wins
+      circles = row.find_all{ |s| self[s] == CIRCLE  }.size 
+      puts "winner: #{row.inspect} circles=#{circles}"
+      return CIRCLE if circles == 3  # "circle" wins
+      crosses = row.find_all{ |s| self[s] == CROSS  }.size   
+      puts "winner: #{row.inspect} crosses=#{crosses}"
+      return CROSS  if crosses == 3
     end
-    return " " if blanks == 0   # tie
     false
   end
 end
 
-CIRCLE = "circle"
-CROSS  = "cross"
+include TicTacToe
 get %r{^/([abc][123])?$} do |human|
   if human then
     puts "You played: #{human}!"
     if BOARD.legal_moves.include? human
       BOARD[human] = CIRCLE
       computer = BOARD.legal_moves.sample
+      redirect to ('humanwins') if BOARD.winner == CIRCLE
       redirect to('/') unless computer
       BOARD[computer] = CROSS
       puts "I played: #{computer}!"
@@ -72,5 +82,11 @@ get %r{^/([abc][123])?$} do |human|
     puts Board::HORIZONTALS.inspect
     BOARD = Board.new(session)
   end
-  erb :game, :locals => { :b => BOARD }
+  erb :game, :locals => { :b => BOARD, :m => ''  }
+end
+
+get '/humanwins' do
+  erb :final, :locals => { :b => BOARD, :m => 'Human wins' }
+  #sleep(2)
+  #redirect to('/')
 end
