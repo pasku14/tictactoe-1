caa97bbf14f395147c2f9b0612a024ef4183be80
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-11-02 15:01:51 +0000

    factorized game.erb

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,6 @@
-
 $(function() {
   $(".cell").click(function(e) {
-    $.get(e.srcElement.id, function(data) {
-      console.log(data);
-      if (data != 'illegal') {
-        $(e.target).addClass("circle");
-        $("#"+data).addClass( "cross" );
-      }
-      else {
-        alert("Illegal move!")
-      }
-      //alert( "Load was performed." );
-    });
+    $.get(e.srcElement.id);
   });
 });
 
